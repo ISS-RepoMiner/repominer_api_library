9835f9817406cac243df99106c50934913654d2c
LeoLee <leo.li@iss.nthu.edu.tw>
2017-03-29 03:06:49 +0800

    ADD vcr, env variable into spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 require 'minitest/autorun'
+require 'yaml'
+require 'vcr'
+require 'webmock'
+
 require_relative '../lib/api_library.rb'
 
-REPO_USER = 'NoBetterThan97'
-REPO_NAME = 'Worldca-api'
-UPDATE_TIME = '2017-01-05T08:17:47Z'
-USER_AGENT = 'Leoli0320'
+FIXTURES_FOLDER = 'fixtures'
+CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
+# Need to Fix
+#===
+CASSETTE_FILE = 'github_api'
+RESULT_FILE = "#{CASSETTES_FOLDER}/github_api_results.yml"
+#===
+if File.file?('../config/credentials.yml')
+  CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
+  ENV['REPO_USER'] = CREDENTIALS[:REPO_USER]
+  ENV['REPO_NAME'] = CREDENTIALS[:REPO_NAME]
+  ENV['USER_AGENT'] = CREDENTIALS[:USER_AGENT]
+  ENV['ACCESS_TOKEN'] = CREDENTIALS[:ACCESS_TOKEN]
+  time = Time.now - 60 * 60 * 24 * 30 * 2
+  ENV['UPDATE_TIME'] = time.to_time.iso8601
+end
+
+
+VCR.configure do |c|
+  c.cassette_library_dir = CASSETTES_FOLDER
+  c.hook_into :webmock
+  c.filter_sensitive_data('<ACCESS_TOKEN>') do
+      URI.unescape(ENV['ACCESS_TOKEN'])
+    end
+  c.filter_sensitive_data('<ACCESS_TOKEN_ESCAPED>') do
+    ENV['ACCESS_TOKEN']
+  end
+end
